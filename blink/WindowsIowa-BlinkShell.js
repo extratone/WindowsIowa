3940da6c590bdabdc16dca70ae1b20906c92ce63
David Blue <davidblue@extratone.com>
2022-11-09 11:04:04 -0600

    #4

---
+++ blink/WindowsIowa-BlinkShell.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#050387", "#ff2320", "#00ff00", "#f5ff6f", "#2934b3", "#1f0022", "#c4f7f9", "#fff3e4", "#000051", "#ff1f1e", "#00ff00", "#f5ff6f", "#6869f3", "#ed0073", "#93fdff", "#ffffff"]);
+t.prefs_.set('foreground-color', "#ffffff");
+t.prefs_.set('background-color', "#00006f");
+t.prefs_.set('cursor-color', "#ffffff");
